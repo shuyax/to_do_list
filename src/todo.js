f0b4b98483fb10c1d0b62c81b85f9602bd79ff86
Mac Mini <macmini@macs-mbp.myfiosgateway.com>
2024-03-24 12:41:34 -0400

    Seperate the todo class and project class from index.html; todo class can only be modified via project class and project class can be modified in index.html

---
+++ src/todo.js
@@ -0,0 +1,42 @@
+export class ToDo {
+    constructor(description, due_date_time  = new Date(8640000000000000), priority = 'low', to_do_status = 'to_do', sub_to_do = {}){
+        this.description = description
+        this.due_date_time = due_date_time
+        this.priority = priority
+        this.to_do_status = to_do_status
+        this.sub_to_do = sub_to_do
+    };
+    get properties (){
+        return {
+            description: this.description,
+            due_date_time: this.due_date_time,
+            priority: this.priority,
+            to_do_status: this.to_do_status,
+            sub_to_do: this.sub_to_do
+        }
+    }
+    set properties ({description, due_date_time, priority, to_do_status, sub_to_do}){
+        if (description != undefined){
+            this.description = description;
+        }
+        if (due_date_time != undefined){
+            this.due_date_time = due_date_time;
+        }
+        if (priority != undefined){
+            this.priority = priority;
+        }
+        if (to_do_status != undefined){
+            this.to_do_status = to_do_status;
+        }
+        if (sub_to_do != undefined){
+            this.sub_to_do = sub_to_do;
+        }
+    }
+    editToDo(description, due_date_time, priority, to_do_status){
+        this.description = description
+        this.due_date_time = due_date_time
+        this.priority = priority
+        this.to_do_status = to_do_status
+
+    }
+};
